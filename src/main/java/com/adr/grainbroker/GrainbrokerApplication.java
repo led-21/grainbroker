2b206315d3f15269b55ee1929f4a6cd070e936ab
adriano <adriano.godoy@gmail.com>
2024-03-25 19:36:16 -0400

    Commit inicial.

---
+++ src/main/java/com/adr/grainbroker/GrainbrokerApplication.java
@@ -0,0 +1,13 @@
+package com.adr.grainbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainbrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrainbrokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adr/grainbroker/GrainbrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adr.grainbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrainbrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
